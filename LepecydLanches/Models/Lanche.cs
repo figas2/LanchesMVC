f2c4583cca46c4235a70b46c9c612db2e166bc36
Matheus <mfigueiredoeng@gmail.com>
2023-07-05 18:00:50 -0300

    Criacao das classes Lanche e Categoria

---
+++ LepecydLanches/Models/Lanche.cs
@@ -0,0 +1,18 @@
+﻿namespace LepecydLanches.Models
+{
+    public class Lanche
+    {
+        public int LancheId { get; set; }
+        public string Nome { get; set; }
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemThumbnailUrl { get; set; }
+        public string ImagemUrl { get; set; }
+        public bool EmEstoque { get; set; }
+        public bool IsLanchePreferido { get; set; }
+
+        public int CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; } //propriedade de navegação
+    }
+}
