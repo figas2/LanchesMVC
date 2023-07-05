5403f4c01a82ca1aa8cd0283ad9d6947cb9ae170
Matheus <mfigueiredoeng@gmail.com>
2023-07-05 15:46:48 -0300

    Criacao Startup e ajuste program

---
+++ LepecydLanches/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using LepecydLanches.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LepecydLanches.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Demo()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
