d0d056eac847ea12a65c3ddf9ef1bea26022abea
Matheus <mfigueiredoeng@gmail.com>
2023-07-06 11:36:18 -0300

    Popular tabela categorias e lanches

---
+++ LepecydLanches/Migrations/20230706135426_PopularCategorias.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LepecydLanches.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert Into Categorias(CategoriaNome,Descricao) " + 
+                "Values('Normal', 'Lanche feito com ingredientes normais')");
+            mb.Sql("Insert Into Categorias(CategoriaNome,Descricao) " +
+                "Values('Natural', 'Lanche feito com ingredientes integrais e naturais')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Categorias");
+        }
+    }
+}
